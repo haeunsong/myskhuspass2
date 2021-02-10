58d955a7878b827e8394216d6ac7e13cefac37f5
haeunsong <jamy0225@naver.com>
2021-02-10 14:26:13 +0900

    로그아웃 기능 + 건물관리 기능(수정필요)

--- src/Components/Screen/Admin/Dashboard/Main.js
+++ src/Components/Screen/Admin/Dashboard/Main.js
@@ -1,18 +1,32 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
 
-const Main = () => {
-  const [areas,setAreas] = useState(null);
-  const [loading,setLoading] = useState(false);
-  const [error,setError] = useState(null);
+import { authService } from '../../../../fbase';
 
-  useEffect(()=>{
-    const getAreas = async() => {
+const Main = (props) => {
+  const [areaList, setAreaList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getAreas = async () => {
       try {
         // 요청이 시작할 때는 error와 areas 초기화
-        setError(null); setAreas(null); setLoading(true);
-        const response = await fetch('전체 건물 조회 api 주소');
-        setAreas(response.data);
-      }catch(e) {
+        setError(null); setAreaList(null); setLoading(true);
+
+        let idToken = await authService.currentUser.getIdToken(true);
+
+        const result = await fetch('/area', {
+          method: "GET",
+          headers: { 'Firebase-ID-Token': idToken }
+        });
+        if (result.ok) {
+          let areas = await result.json();
+          setAreaList(areas);
+          console.log(setAreaList);
+        }
+        // setAreas(response.data);
+      } catch (e) {
         setError(e);
         console.log(e);
       }
@@ -20,25 +34,51 @@ const Main = () => {
     };
     getAreas();
 
-  },[]);
+  }, []);
+
+  const onLogoutHandler = async () => {
+    try {
+      let result = await authService.signOut();
+      console.log(result);
+      props.history.push('/admin');
+
+    } catch (err) {
+      alert(err);
+
+    }
+  }
 
   if (loading) return <div>로딩중....</div>
   if (error) return <div>에러</div>
-  if (!areas) return <div>등록된 건물이 없습니다.</div>
-  
+  if (!areaList) return (
+    <>
+      <div>등록된 건물이 없습니다.</div>
+      <Button onClick={onLogoutHandler}>로그아웃</Button>
+
+    </>
+  )
+
+  const addAreaHandler = () => {
+    
+  }
+
   return (
-    <div>
+    <>
+
       등록된 건물 목록 표시 화면(전체 건물 조회)
       <ul>
-        {areas.map(area => (
+        {/* {areaList.map(area => (
           <li key={area.id}>
             {area.areaname}
           </li>
-        ))}
+        ))} */}
       </ul>
+      <Button onClick={onLogoutHandler}>로그아웃</Button>
 
-      
-    </div>
+      <Button onClick={addAreaHandler} >건물추가</Button>
+
+
+    </>
   );
 };
 
